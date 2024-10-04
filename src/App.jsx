20b7d4bf7655b4ee36514a154bac7b4057a8b6fc
StivCode <stivenelcodificador@gmail.com>
2024-10-03 21:08:10 -0500

    includes sales components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Hero } from './components'
-import { heroapi } from './data/data'
+import { Hero, Sales } from './components'
+import { heroapi, popularsales, toprateslaes } from './data/data'
 
 
 const App = () => {
   return (
     <>
-      <main>
+      <main className='flex flex-col gap-16 relative'>
         <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} />
+        <Sales endpoint={toprateslaes} />
       </main>
     </>
   )
